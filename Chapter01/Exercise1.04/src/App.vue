79cbcfedcbde50c143862b5ded0dc73fc8a26bdf
Jefferson Tales Canalli <jefferson.canalli@hotmail.com>
2025-11-12 20:16:12 -0300

    feat(excs-1-04): completed

--- Chapter01/Exercise1.04/src/App.vue
+++ Chapter01/Exercise1.04/src/App.vue
@@ -4,6 +4,7 @@
 
 <script>
 import Exercise from "./components/Exercise1-04.vue";
+// import Exercise from "./components/Exercise1-04-jeff.vue";
 
 export default {
   components: {
---
+++ Chapter01/Exercise1.04/src/components/Exercise1-04-jeff.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <label>
+            Name
+            <input 
+                type="text" 
+                v-model="name"
+            />
+        </label>
+         <label>
+            Langague
+            <input 
+                type="text" 
+                v-model="language"
+            />
+        </label>
+        <div>
+            <h2>Name: {{ name }}</h2>
+            <h2>Language: {{ language }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            name: "",
+            language: "",
+        }
+    }
+}
+</script>
